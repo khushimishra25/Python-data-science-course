73405bf402c72bca5a35d837650fa4f5dac36d9f
khushimishra25 <mishrakhushi6002@gmail.com>
2022-11-07 21:03:22 +0530

    stringspractice

--- si.py
+++ si.py
@@ -1,10 +1,15 @@
-#p=float(input("Enter principle:"))
-#r=float(input("Enter rate:"))
-#t=float(input("Enter time:"))
-#si=(p*r*t)/100
-#print("Simple interest =",si)
+p=float(input("Enter principle:"))
+r=float(input("Enter rate:"))
+t=float(input("Enter time:"))
+si=(p*r*t)/100
+print("Simple interest =",si)
 
 c=float(input("Enter the circumferene :"))
 r=c/(3.14*2)
 area=3.14*(r**2)
 print("Area of circle =",area)
+
+
+
+
+
